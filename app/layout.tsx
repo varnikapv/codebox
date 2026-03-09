52db5af9772d36a8934f1ae25fa83ed728f32fb5
Varnika <pvarnika05@gmail.com>
2026-03-09 10:00:39 +0530

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Pixelify_Sans, Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,6 +13,16 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const GameFont = Pixelify_Sans({
+  subsets:['latin'],
+  variable:'--font-game',
+})
+
+const inter = Inter({
+  subsets:['latin'],
+  variable:'--font-inter',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,9 +35,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", inter.variable)}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} 
+        ${GameFont.variable}   ${inter.variable}     antialiased`}
       >
         {children}
       </body>
