0c3ca24a9fa8bc685ce35bd64ef8ef90e1b3dcdb
Varnika <pvarnika05@gmail.com>
2026-03-14 16:31:39 +0530

    all-courses page implementation

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,13 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'ik.imagekit.io',
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        port: "",
+        pathname: "/**",
       },
     ],
+    formats: ["image/webp", "image/avif"],  // ✅ explicitly allow webp
   },
 };
 
